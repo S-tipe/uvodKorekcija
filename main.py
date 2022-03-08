26f92ce7163e88abb2dae38bd4a562937596184f
Stipe Roguljić <stipe.roguljic@gmail.com>
2022-03-08 17:13:09 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#ovo je naš prvi py program
+
+"""mood = "silly"
+
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "anxious":
+    print("Do not worry, be happy!")
+else:
+    print("Cheer up, mate!")"""
+
+ime=input("upiši svoje ime: ")
+mood=input("kako se osjećaš? ")
+print("Hej, "+ime+" nemoj biti "+mood)
